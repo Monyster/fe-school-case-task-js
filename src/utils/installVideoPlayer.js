c6d76f461aa3b4cdf4fb88b14f053eda995aa0d0
Matvii Voznosimenko <main.monyster@gmail.com>
2023-03-19 19:02:44 +0200

    adding comments

--- src/utils/installVideoPlayer.js
+++ src/utils/installVideoPlayer.js
@@ -5,17 +5,7 @@ export const installVideoPlayer = (ref, videoLink) => {
     var hls = new Hls();
     hls.loadSource(videoLink);
     hls.attachMedia(ref.current);
-    // hls.on(Hls.Events.MANIFEST_PARSED, function () {
-    //   videoPlayer.play();
-    // });
   } else if (ref.current.canPlayType("application/vnd.apple.mpeg")) {
     ref.current.src = videoLink;
-    // videoPlayer.addEventListener("canplay", function () {
-    //   videoPlayer.play();
-    // });
   }
-
-  // ref.current.addEventListener("error", (event) => {
-  //   console.log(event);
-  // });
 };
